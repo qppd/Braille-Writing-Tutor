ae507a8ed7d652cf3435526d319c9219b75d0d58
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-09-13 15:09:03 +0800

    Updated README

---
+++ source/rpi/BrailleWritingTutor/main.py
@@ -0,0 +1,7 @@
+# Minimal Python script for Braille Writing Tutor (Raspberry Pi)
+
+def main():
+    print("Braille Writing Tutor (Raspberry Pi) is running.")
+
+if __name__ == "__main__":
+    main()
